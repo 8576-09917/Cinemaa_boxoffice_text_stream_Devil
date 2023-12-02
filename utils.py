fe99f702a25029a5a916f227d882b5f049ebbe54
Shivu143mm <132443261+Shivu143mm@users.noreply.github.com>
2023-12-03 01:03:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -733,7 +733,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                                 InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                 InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                             ],[
-                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Kgashok04")
+                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/update143")
                                 ]
                             ]
                         )
